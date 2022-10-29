cdfdf5afec3d2e3af09ac23bf1f24643f00298cb
andybrady52 <andybrady52@gmail.com>
2022-10-29 23:24:57 +0100

    first commit

---
+++ createLocalAdminUser.ps1
@@ -0,0 +1,14 @@
+﻿echo "Please enter the new user name "
+$userName = Read-Host
+echo "Please enter the new user password "
+$password = Read-Host -AsSecureString
+echo "Which user group would you like to add the user to? "
+$userGroup = Read-Host
+
+echo "Please enter the new user description "
+$userDescription = Read-Host
+
+echo "Creating new user...."
+New-LocalUser -Name $userName -Password $password -FullName $password -Description $userDescription
+echo "Adding user to " $userGroup " group "
+Add-LocalGroupMember -Group $userGroup -Member $userName
