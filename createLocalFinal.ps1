80fba48271b6f4a34fe85bca815795262e7b558f
andybrady52 <andybrady52@gmail.com>
2022-11-05 02:08:26 +0000

    first commit on CSV functionality

--- createLocalFinal.ps1
+++ createLocalFinal.ps1
@@ -1,4 +1,5 @@
-﻿function Show-Menu
+﻿#Define menu
+function Show-Menu
 {
     param (
         [string]$Title = 'My Menu'
@@ -14,48 +15,74 @@
 
 
 
-#Enter user details 
+#Include option to create users from CSV file
 
-$userName = Read-Host -Prompt "Please enter the new user name "
-DO{
-    $pwd1 = Read-Host -Prompt "Enter Password" -AsSecureString
-    $pwd2 = Read-Host -Prompt "Confirm Password" -AsSecureString
+$CSV? = Read-Host -Prompt "Would you like to create users from a CSV file? y/n"
 
-    $pwd1_text = [Runtime.InteropServices.Marshal]::PtrToStringAuto([Runtime.InteropServices.Marshal]::SecureStringToBSTR($pwd1))
-    $pwd2_text = [Runtime.InteropServices.Marshal]::PtrToStringAuto([Runtime.InteropServices.Marshal]::SecureStringToBSTR($pwd2))
-    if ($pwd1_text -eq $pwd_text2){
-        $pwd_final = $pwd2
-     }
+if ($CSV? -eq "y")
+{
 
-    if ($pwd1_text -ne $pwd2_text){
+    
 
-        Write-Host "Passwords do not match, please re enter"
+    $filepath = Read-Host -Prompt "Please enter filepath for the CSV file"
+
+    $CSV = Import-Csv $filepath
+
+
+    foreach($LINE in $CSV)
+    {
+        $NewUser="$($LINE.USERNAME)"
+        $NewPass="$($LINE.PASSWORD)" 
+        $SecurePass=ConvertTo-SecureString –AsPlainText -Force -String "$NewPass"
+        New-LocalUser -Name $NewUser -Password $SecurePass
+        write-host $NewUser" Created"
     }
-}while ($pwd1_text -ne $pwd2_text)
+}else {
+
+    #Enter user details 
+
+    $userName = Read-Host -Prompt "Please enter the new user name "
+    DO{
+        $pwd1 = Read-Host -Prompt "Enter Password" -AsSecureString
+        $pwd2 = Read-Host -Prompt "Confirm Password" -AsSecureString
+
+        $pwd1_text = [Runtime.InteropServices.Marshal]::PtrToStringAuto([Runtime.InteropServices.Marshal]::SecureStringToBSTR($pwd1))
+        $pwd2_text = [Runtime.InteropServices.Marshal]::PtrToStringAuto([Runtime.InteropServices.Marshal]::SecureStringToBSTR($pwd2))
+        if ($pwd1_text -eq $pwd_text2){
+            $pwd_final = $pwd2
+        }
+
+        if ($pwd1_text -ne $pwd2_text){
+
+            Write-Host "Passwords do not match, please re enter"
+        }
+    }while ($pwd1_text -ne $pwd2_text)
 
 
 
 
 
-Show-Menu –Title 'My Menu'
- $selection = Read-Host "Please make a selection"
- switch ($selection)
- {
-     '1' {
-         $userGroup = "Administrators"
-     } '2' {
-         $userGroup = "Users"
-         
-     } 'q' {
-         return
-     }
- }
+    Show-Menu –Title 'My Menu'
+    $selection = Read-Host "Please make a selection"
+    switch ($selection)
+    {
+        '1' {
+            $userGroup = "Administrators"
+        } '2' {
+            $userGroup = "Users"
+            
+        } 'q' {
+            return
+        }
+    }
 
 
-echo "Please enter the new user description "
-$userDescription = Read-Host
+    echo "Please enter the new user description "
+    $userDescription = Read-Host
 
-write-host "Creating "$userName
-New-LocalUser -Name $userName -Password $pwd2 -FullName $userName -Description $userDescription
-write-host "Added user to " $userGroup " group "
-Add-LocalGroupMember -Group $userGroup -Member $userName
+    write-host "Creating "$userName
+    New-LocalUser -Name $userName -Password $pwd2 -FullName $userName -Description $userDescription
+    write-host "Added user to " $userGroup " group "
+    Add-LocalGroupMember -Group $userGroup -Member $userName
+    "";""
+    }
